0a6e39cd649baf77a27d597e973d7a2dffe653b2
cookiecollie <ediblechocolatething@gmail.com>
2024-02-09 17:32:27 +0700

    update: Add Card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,6 +1,20 @@
-interface CardProps extends React.PropsWithChildren {}
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+    imgSrc?: string
+}
 
 export const Card = (props: CardProps) => {
-    const { children } = props
-    return <div>{children}</div>
+    const { children, imgSrc, ...others } = props
+    return (
+        <div
+            {...others}
+            className="flex flex-col gap-4 rounded-2xl bg-secondary p-4"
+        >
+            {imgSrc && (
+                <div className="overflow-hidden rounded-2xl">
+                    <img src={imgSrc} className="object-cover" />
+                </div>
+            )}
+            <div>{children}</div>
+        </div>
+    )
 }
--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -3,7 +3,24 @@ import { Card } from "../../components/card"
 export const Dashboard = () => {
     return (
         <>
-            <Card>Card</Card>
+            <div className="grid grid-cols-5 gap-x-6 gap-y-8 p-6 px-12">
+                {Array(30)
+                    .fill(0)
+                    .map((e, i) => (
+                        <Card
+                            key={`card-${i}`}
+                            imgSrc="https://static-cdn.jtvnw.net/jtv_user_pictures/b5235413-c5b7-4b70-9444-b5de000a846d-profile_image-300x300.png"
+                        >
+                            <div className="flex flex-col gap-4">
+                                <p className="text-center text-2xl font-medium">
+                                    cookiecollie
+                                </p>
+
+                                <div>XP</div>
+                            </div>
+                        </Card>
+                    ))}
+            </div>
         </>
     )
 }
